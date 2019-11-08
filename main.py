2a100d56f1472f18b83825f24b6a90ac0bf9cc4f
Will Chang <williamchang6@gmail.com>
2019-11-07 21:59:25 -0800

    Adding lesson01 assignment files and updates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation 
 
 app = Flask(__name__)
 
@@ -16,6 +16,17 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        new_donor = Donor(name=request.form['name'])
+        new_donor.save()
+        new_donation = Donation(donor=new_donor, value=int(request.form['value']))
+        new_donation.save()
+        
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
